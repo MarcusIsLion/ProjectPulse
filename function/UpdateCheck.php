1a5511c5e09e623c681cae6880cfe3cf7ef9bb3b
Mathis VINCENT <mathis.vincent@iut-dijon.u-bourgogne.fr>
2024-09-07 21:43:46 +0200

    Adding the documentation of the function files

--- function/UpdateCheck.php
+++ function/UpdateCheck.php
@@ -1,42 +1,59 @@
 <?php
+// This file is used to check if the version of the project is up to date or not.
 
-// Fonction pour récupérer le contenu JSON à partir d'une URL
+///<sumary>
+/// Get the JSON content from an URL
+///</sumary>
+///<param name="$url">The URL of the JSON file</param>
+///<returns>The JSON content</returns>
+///<exception>Exception if the file can't be retrieved</exception>
 function getJsonFromUrl($url)
 {
     $json = file_get_contents($url);
     if ($json === false) {
-        throw new Exception("Impossible de récupérer le fichier JSON depuis l'URL : $url");
+        throw new Exception("Impossible to get the file at the URL : $url");
     }
     return json_decode($json, true);
 }
 
-// Fonction pour récupérer le contenu JSON à partir d'un fichier local
+///<sumary>
+/// Get the JSON content from a file
+///</sumary>
+///<param name="$filePath">The path of the JSON file</param>
+///<returns>The JSON content</returns>
+///<exception>Exception if the file doesn't exist</exception>
 function getJsonFromFile($filePath)
 {
     if (!file_exists($filePath)) {
-        throw new Exception("Le fichier local n'existe pas : $filePath");
+        throw new Exception("The local file doesn't exist at the path : $filePath");
     }
     $json = file_get_contents($filePath);
     return json_decode($json, true);
 }
 
+///<sumary>
+/// Check if the version of the project is up to date
+///</sumary>
+///<returns>True if the version is up to date, false otherwise</returns>
+///<exception>Exception if an error occurs</exception>
 function CheckVersions()
 {
     try {
-        // URL du fichier JSON sur GitHub
+        // URL of the version.json file on GitHub
         $githubJsonUrl = 'https://raw.githubusercontent.com/MarcusIsLion/ProjectPulse/main/data/version.json';
 
-        // Chemin du fichier local version.json
+        // Path of the version.json file on the local
         $localJsonPath = 'data/version.json';
-        // Récupération de la version depuis l'URL GitHub
+
+        // Getting the version from the GitHub file
         $githubData = getJsonFromUrl($githubJsonUrl);
         $githubVersion = $githubData['version'];
 
-        // Récupération de la version depuis le fichier local
+        // Getting the version from the local file
         $localData = getJsonFromFile($localJsonPath);
         $localVersion = $localData['version'];
 
-        // Comparaison des versions
+        // Comparing the versions
         if ($githubVersion === $localVersion) {
             return true;
         } else {
