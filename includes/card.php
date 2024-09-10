feb6adba67c823813b1e31b3d9ccb2d54151f137
Mathis VINCENT <mathis.vincent@iut-dijon.u-bourgogne.fr>
2024-09-10 08:30:51 +0200

    Adding a fabric of card to be more efficient

---
+++ includes/card.php
@@ -0,0 +1,59 @@
+<?php
+function generateCardHTML($logo, $dossier, $chemin_complet, $state, $LanguageIdPopover)
+{
+    // Récupération des informations de taille, dates, et type
+    $size = formatSizeUnits(getFolderSize($chemin_complet));
+    $createdAt = date("d/m/Y", filectime($chemin_complet));
+    $updatedAt = date("d/m/Y", filemtime($chemin_complet));
+    $fileCount = count(scandir($chemin_complet));
+    $folderCount = count(glob($chemin_complet . '/*', GLOB_ONLYDIR));
+
+    // Lecture des informations du fichier JSON
+    $typeData = json_decode(file_get_contents($chemin_complet . "/type.json"));
+    $type = $typeData->type;
+    $language = $typeData->language;
+    $stateFromJson = $typeData->state;
+
+    // Construction du HTML à retourner
+    $html = '<div class="card">';
+
+    if ($logo) {
+        $html .= '<div class="logo-background" style="background-image: url(\'' . htmlspecialchars($logo, ENT_QUOTES) . '\');"></div>';
+    }
+
+    $html .= '<div class="CardContent">';
+    $html .= '<h2>' . htmlspecialchars($dossier, ENT_QUOTES) . '</h2>';
+    $html .= '<ul>';
+    $html .= '<li>
+                <p>Size : ' . $size . '</p>
+            </li>';
+    $html .= '<li>
+                <p>Created at : ' . $createdAt . '</p>
+            </li>';
+    $html .= '<li>
+                <p>Updated at : ' . $updatedAt . '</p>
+            </li>';
+    $html .= '<li>
+                <p>' . $fileCount . ' files / ' . $folderCount . ' folders</p>
+            </li>';
+    $html .= '<li>
+                <p>Type : ' . $type . '</p>';
+    $html .= '<div class="icon-container">';
+    $html .= '<button onclick="afficherPopover(\'LanguageIdPopover' . $LanguageIdPopover . '\')" class="NotButton"><i class="fas fa-info-circle popover-icon"></i></button>';
+    $html .= '<div class="popover hidden" id="LanguageIdPopover' . $LanguageIdPopover . '">' . $language . '</div>';
+    $html .= '</div>
+            </li>';
+    $html .= '<li class="' . htmlspecialchars($state, ENT_QUOTES) . '">';
+    $html .= '<p>State : ' . $stateFromJson . '</p>';
+    $html .= '</li>
+        </ul>';
+
+    $html .= '<div class="CardButtonSection">';
+    $html .= '<a href="' . htmlspecialchars($chemin_complet, ENT_QUOTES) . '" class="button">Open this project</a>';
+    $html .= '<a href="page/UpdateProject.php?ProjectName=' . htmlspecialchars($chemin_complet, ENT_QUOTES) . '" class="button">Update these characteristics</a>';
+    $html .= '</div>
+    </div>
+</div>';
+
+    return $html;
+}
